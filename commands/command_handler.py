abe5ed06ba905f12168ef3ba09e24ad1508ebbd7
YYYY <ysaq3424@gmail.com>
2025-10-14 12:32:21 +0900

    update : Discord Serverに参加したら挨拶をするように実装
    Discord serverに参加した時点でサーバIDを取得する。取得したIDはSQLに投げるように実装する。

--- commands/command_handler.py
+++ commands/command_handler.py
@@ -1,8 +1,11 @@
 # コマンド処理
+import discord
 from discord.ext import commands
-from discord import Intents, Interaction
-from discord.app_commands import CommandTree
+from discord import Intents
 from reminder import Reminder
+
+GREETINGS_TEXT = "ブンブンハローユーチューブ！どうもJapaneseKoreanUGTVです！こんにちはー！"
+GREETINGS_URL = "https://www.twitch.tv/alfrea/clip/TriangularCogentLEDSwiftRage-93khMrEhJ9If7N5g"
 
 
 class CommandHandler(commands.Bot):
@@ -21,3 +24,10 @@ class CommandHandler(commands.Bot):
     async def on_ready(self):
         await self.tree.sync()
         print(f'Logged in as {self.user}')
+
+    async def on_guild_join(self, guild: discord.Guild):
+        server_id = guild.id
+        # Botがメッセージ送信可能な最初のテキストチャンネルを取得
+        channel = next((c for c in guild.text_channels if c.permissions_for(guild.me).send_messages), None)
+        if channel:
+            await channel.send(f"{GREETINGS_TEXT}\n{GREETINGS_URL}")
